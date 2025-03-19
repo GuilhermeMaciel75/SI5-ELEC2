a0daf16629136746f6f31d10f1e94a491abaafc2
Rubens Lima <rubensndelima@hotmail.com>
2025-03-18 21:54:04 -0300

    update search_params

--- lib/runner.py
+++ lib/runner.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score, f1_score, roc_curve, auc, recall_score
+from sklearn.metrics import accuracy_score, f1_score, roc_curve, roc_auc_score, recall_score
 import numpy as np
 import time
 import pandas as pd
@@ -8,21 +8,24 @@ from lib.util import (
     RANDOM_STATE, RESULTS_PATH, RESULTS_DIR, 
     load_json, save_json, save_model, load_model, save_results
 )
+import os
+import pickle
 
 # Função para avaliar o modelo com base em várias métricas no conjunto de validação
-def evaluate_model_metrics(model, X_val, Y_val):
+def evaluate_model_metrics(model, X_val, Y_val, test=False):
     Y_pred = model.predict(X_val)
-    
-    fpr, tpr, _ = roc_curve(Y_val, Y_pred)
     
     metrics = {
         'accuracy': accuracy_score(Y_val, Y_pred),
         'f1_score': f1_score(Y_val, Y_pred),
         'recall': recall_score(Y_val, Y_pred),
-        'auc': auc(fpr, tpr),
-        'fpr': list(fpr),
-        'tpr': list(tpr),
+        'auc': roc_auc_score(Y_val, Y_pred),
     }
+    
+    if test:
+        fpr, tpr, _ = roc_curve(Y_val, Y_pred)
+        metrics['fpr'] = list(fpr),
+        metrics['tpr'] = list(tpr)
     
     return metrics
 
@@ -187,3 +190,154 @@ def new_search_params(model, params, model_name,
         save_json(models_results, RESULTS_PATH)
 
     return df_cvs, best_model, results, df_iter
+
+def cv_best_model(model,params,  X_val, Y_val):
+    kfold = StratifiedKFold(n_splits=5, random_state=RANDOM_STATE, shuffle=True)
+    
+    cv_metrics = []
+    
+    for fold, (train_idx, val_idx) in tqdm(enumerate(kfold.split(X_val, Y_val), 1), desc=f"Realizando CV dos parâmetros {params}"):
+        X_train_fold, X_val_fold = X_val[train_idx], X_val[val_idx]
+        Y_train_fold, Y_val_fold = Y_val[train_idx], Y_val[val_idx]
+        
+        # Treina o modelo
+        model.set_params(**params)
+        model.fit(X_train_fold, Y_train_fold)
+        
+        # Avalia o modelo no conjunto de validação do fold
+        fold_metrics = evaluate_model_metrics(model, X_val_fold, Y_val_fold)
+        fold_metrics['fold'] = fold
+        cv_metrics.append(fold_metrics)
+    
+    return cv_metrics
+
+def search_params(model, params, model_name, dataset):
+    
+    X_train, Y_train, X_val, Y_val, X_test, Y_test = map(np.array, dataset)
+    
+    best_params = {}    
+    find_models  = 0
+    
+    MODEL_PATH = f'./models/{model_name}'
+    
+    for i in tqdm(range(20), desc="Realizando a Busca de Parâmetros por 20 iteraçòes"):
+        search_model = RandomizedSearchCV(
+            estimator=model,
+            param_distributions=params,
+            cv=5,
+            scoring='roc_auc',
+            n_iter=20,
+            random_state=RANDOM_STATE + i,
+            n_jobs=-1,
+            refit=True
+        )
+        
+        search_model.fit(X_train, Y_train)
+        
+        # Se o mesmo conjunto de parâmetros foi escolhiodo anteriormente ignora
+        if (params_str :=str(search_model.best_params_)) in best_params:
+            continue
+        
+        find_models += 1
+                
+        model_data = {
+            "params" : search_model.best_params_,
+            "score" : search_model.best_score_,
+            "model_time" : search_model.refit_time_,
+            "train_score" : evaluate_model_metrics(search_model.best_estimator_, X_test, Y_test,True),
+            "validation_matrix" : cv_best_model(model, search_model.best_params_, X_val, Y_val),
+            "model_path" : f"./models/{model_name}/{find_models}.pkl"
+        }
+        
+        best_params[params_str] = model_data
+        
+        if not os.path.exists(MODEL_PATH):
+            os.makedirs(MODEL_PATH)
+        
+        with open(f"{MODEL_PATH}/{find_models}.pkl", 'wb') as file:
+            pickle.dump(search_model.best_estimator_, file)
+    
+    save_json(best_params, f"{MODEL_PATH}/{model_name}.json")
+    
+    return best_params
+        
+        
+
+def search_paramsv2(model, params, model_name, dataset, n_iter=20, score='auc'):
+    
+    X_train, Y_train, X_val, Y_val, X_test, Y_test = map(np.array, dataset)
+    
+    best_params = {}
+    best_model = None
+    best_score = -float('inf')
+    best_cv = None
+    all_cv = pd.DataFrame()
+    
+    MODEL_PATH = f'./models/{model_name}'
+    
+    for i in tqdm(range(20), desc="Realizando a Busca de Parâmetros por 20 iterações"):
+        
+        search_model = RandomizedSearchCV(
+            estimator=model,
+            param_distributions=params,
+            cv=5,
+            scoring='roc_auc',
+            n_iter=20,
+            random_state=RANDOM_STATE + i,
+            n_jobs=-1,
+            refit=True,
+            return_train_score=True
+        )
+        
+        search_model.fit(X_train, Y_train)
+        
+        all_cv = pd.concat([all_cv, pd.DataFrame(search_model.cv_results_).iloc[search_model.best_index_]], axis=1)
+                
+        Y_pred_train = search_model.best_estimator_.predict(X_train)
+        Y_pred_val = search_model.best_estimator_.predict(X_val)
+        Y_pred_test = search_model.best_estimator_.predict(X_test)
+        
+        fpr, tpr, _ = roc_curve(Y_test, Y_pred_test)
+            
+        best_params[i] = {
+            "params" : search_model.best_params_,
+            "score" : search_model.best_score_,
+            "model_time" : search_model.refit_time_,
+            
+            'accuracy_train': accuracy_score(Y_train, Y_pred_train),
+            'accuracy_val' : accuracy_score(Y_val, Y_pred_val),
+            'accuracy_test': accuracy_score(Y_test, Y_pred_test),
+            
+            'f1_score': f1_score(Y_test, Y_pred_test),
+            'recall': recall_score(Y_test, Y_pred_test),
+            'auc': roc_auc_score(Y_test, Y_pred_test),
+            
+            'fpr': list(fpr),
+            'tpr': list(tpr),
+            
+            "model_path" : f"./models/{model_name}/{i}.pkl"
+        }
+        
+        if best_params[i].get(score) > best_score:
+            best_score = best_params[i].get(score)
+            best_model = search_model.best_estimator_
+            best_cv = pd.DataFrame(search_model.cv_results_).iloc[search_model.best_index_]
+            best_loss = search_model.best_estimator_.loss_curve_ \
+                if hasattr(search_model.best_estimator_, "loss_curve_") else None
+
+        
+        if not os.path.exists(MODEL_PATH):
+            os.makedirs(MODEL_PATH)
+        
+        with open(f"{MODEL_PATH}/{i}.pkl", 'wb') as file:
+            pickle.dump(search_model.best_estimator_, file)
+    
+    save_json(best_params, f"{MODEL_PATH}/{model_name}.json")
+    
+    rows_to_select = [row for row in all_cv.index if 'split' in row and 'score' in row]
+    subset_cv = all_cv.loc[rows_to_select]
+    subset_cv
+    mean_scores = subset_cv.mean(axis=1)
+    mean_scores
+        
+    return best_params, best_model, best_cv, best_loss, mean_scores
--- lib/util.py
+++ lib/util.py
@@ -1,7 +1,7 @@
 import os
 import json
 import pickle
-
+import numpy as np
 
 RANDOM_STATE = 51
 RESULTS_PATH = "./data/results/models.json"
@@ -37,12 +37,9 @@ def load_model(model_name):
             return pickle.load(f)
     return None
     
-def save_results(df_results, model_name, df_iter):
+def save_results(df_results, model_name):
     results_path = f"./data/results/{model_name}.csv"
     df_results.to_csv(results_path, index=False)
-    if df_iter is not None:
-        iter_path = f"./data/results/{model_name}_iter.csv"
-        df_iter.to_csv(iter_path, index)
     return results_path
 
 def show_results(results:dict):
@@ -54,3 +51,42 @@ def show_results(results:dict):
     print("Tempo de Treinamento: {:.3f}s".format(results['fit_execution_time']))
     print("Média de Memória Utilizada: {:.2f} MB".format(results['memory_avg']))
     print("Pico de Memória Utilizada: {:.2f} MB".format(results['memory_max']))
+
+def generate_markdown_report(best_model, results_list):
+    # Inicializando o markdown
+    markdown = f"### Resultados do Modelo\n\n"
+    
+    # Obter os melhores parâmetros do modelo
+    best_params = best_model.get_params()
+    markdown += f"**Melhores Parâmetros:**\n```\n{best_params}\n```\n\n"
+    
+    # Definindo as métricas a serem analisadas
+    metrics = ["accuracy_train", "accuracy_val", "accuracy_test", "f1_score", "auc", "recall"]
+    
+    # Encontrar o modelo com o melhor AUC
+    best_model_row = max(results_list, key=lambda x: x['auc'])  # Modelo com o maior AUC
+    
+    # Iterando sobre as métricas para calcular os valores
+    markdown += "#### Resultados das Métricas:\n"
+    for metric in metrics:
+        # Obtendo os valores para cada métrica
+        all_values = [model[metric] for model in results_list]
+        
+        # Calcular o melhor valor da métrica (máximo)
+        best_value = max(all_values)
+        
+        # Calcular a média e desvio padrão da métrica
+        avg_value = np.mean(all_values)
+        std_value = np.std(all_values)
+        
+        # Encontrar o valor do modelo selecionado (o modelo com o melhor AUC)
+        model_value = best_model_row[metric]
+        
+        # Adicionando os resultados ao markdown
+        markdown += f"- **{metric.replace('_', ' ').capitalize()}**:\n"
+        markdown += f"  - **Melhor valor geral**: {best_value:.4f}\n"
+        markdown += f"  - **Média**: {avg_value:.4f}\n"
+        markdown += f"  - **Desvio Padrão**: {std_value:.4f}\n"
+        markdown += f"  - **Valor no Modelo Selecionado**: {model_value:.4f}\n\n"
+
+    return markdown
